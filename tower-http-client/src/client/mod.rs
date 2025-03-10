8afadf8bdcc46130d6cbb91f60c35d5473c6cb84
Aleksey Sidorov <sauron1987@gmail.com>
2025-03-10 22:10:24 +0300

    Replaced `body_reader` module by one from the http-body-reader crate

--- tower-http-client/src/client/mod.rs
+++ tower-http-client/src/client/mod.rs
@@ -1,18 +1,16 @@
-//! Extensions for Tower services that provide HTTP clients implementation.
+//! Utilities to turn Tower service into the useful HTTP client.
 
 pub use self::{
-    body_reader::BodyReader,
+    body_reader::{BodyReader, ResponseExt},
     client_request::{ClientRequest, ClientRequestBuilder},
     into_uri::IntoUri,
     request_ext::RequestBuilderExt,
-    response_ext::ResponseExt,
     service_ext::ServiceExt,
 };
 
-pub mod body_reader;
 pub mod request_ext;
+pub use http_body_reader as body_reader;
 
 mod client_request;
 mod into_uri;
-mod response_ext;
 mod service_ext;
