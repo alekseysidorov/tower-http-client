28008fb82ace62b1ad3533b12542fe956f6f7dd7
Aleksey Sidorov <sauron1987@gmail.com>
2025-12-07 22:49:50 +0300

    Bump MSRV to 1.89 and refactor flake.nix
    Inline treefmt configuration into flake.nix and remove treefmt.nix. Add
    treefmt.toml to .gitignore. Adjust CI/devShell layout and toolchain
    setup (updated MSRV sha256)

--- flake.nix
+++ flake.nix
@@ -7,149 +7,170 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , fenix
-    , treefmt-nix
-    }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      # Minimum supported Rust version
-      msrv = {
-        name = "1.88.0";
-        sha256 = "sha256-Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk=";
-      };
-      # Setup nixpkgs
-      pkgs = import nixpkgs {
-        inherit system;
-
-        overlays = [
-          fenix.overlays.default
-          (final: prev: {
-            rustToolchains = {
-              stable = prev.fenix.stable.completeToolchain;
-              msrv = (prev.fenix.fromToolchainName msrv).defaultToolchain;
-              nightly = (prev.fenix.complete.withComponents [ "rustfmt" ]);
-            };
-          })
-        ];
-      };
-      # Setup runtime dependencies
-      runtimeInputs = with pkgs; [
-        cargo-nextest
-        openssl
-        pkg-config
-      ];
-
-      # Eval the treefmt modules from ./treefmt.nix
-      treefmt = (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
-      # CI scripts
-      ci = with pkgs; {
-        tests = writeShellApplication {
-          name = "ci-run-tests";
-          runtimeInputs = with pkgs; [ rustToolchains.msrv ] ++ runtimeInputs;
-          text = ''
-            cargo nextest run --workspace --all-targets --no-default-features
-            cargo nextest run --workspace --all-targets --all-features
-
-            cargo test --workspace --doc --no-default-features
-            cargo test --workspace --doc --all-features
-
-            cargo run --example rate_limiter
-            cargo run --example retry
-          '';
-        };
-
-        lints = writeShellApplication {
-          name = "ci-run-lints";
-          runtimeInputs = with pkgs; [ rustToolchains.stable typos ] ++ runtimeInputs;
-          text = ''
-            typos
-            cargo clippy --workspace --all --no-default-features
-            cargo clippy --workspace --all --all-targets --all-features
-            cargo doc --workspace --no-deps --no-default-features
-            cargo doc --workspace --no-deps --all-features
-          '';
-        };
-
-        benchmarks = writeShellApplication {
-          name = "ci-run-benchmarks";
-          runtimeInputs = with pkgs; [ rustToolchains.stable ] ++ runtimeInputs;
-          text = ''
-            cargo bench --workspace --all-features
-          '';
-        };
-
-        semver_checks = writeShellApplication {
-          name = "ci-run-semver-checks";
-          runtimeInputs = with pkgs; [
-            rustToolchains.msrv
-            cargo-semver-checks
-          ] ++ runtimeInputs;
-          text = ''cargo semver-checks'';
-        };
-
-        # Run them all together
-        all = writeShellApplication {
-          name = "ci-run-all";
-          runtimeInputs = [ ci.lints ci.tests ];
-          text = ''
-            ci-run-lints
-            ci-run-tests
-            ci-run-semver-checks
-          '';
-        };
-      };
-
-      mkCommand = shell: command:
-        pkgs.writeShellApplication {
-          name = "cmd-${shell}-${command}";
-          runtimeInputs = [ pkgs.nix ];
-          text = ''nix develop ".#${shell}" --command "${command}"'';
-        };
-
-      mkCommandDefault = mkCommand "default";
-    in
     {
-      # for `nix fmt`
-      formatter = treefmt.wrapper;
-      # for `nix flake check`
-      checks.formatting = treefmt.check self;
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # Common nix packages
+        pkgs = nixpkgs.legacyPackages.${system};
+        # Fenix Rust toolchains
+        fenixPackage = fenix.packages.${system};
+        # Minimum supported Rust version
+        msrv = {
+          name = "1.89.0";
+          sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+        };
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; runtimeInputs ++ [
-          rustToolchains.stable
-          ci.all
-          ci.lints
-          ci.tests
-          ci.semver_checks
-          ci.benchmarks
+        rustToolchains = {
+          stable = fenixPackage.stable.completeToolchain;
+          msrv = (fenixPackage.fromToolchainName msrv).defaultToolchain;
+          nightly = fenixPackage.complete.withComponents [ "rustfmt" ];
+        };
+
+        # Setup runtime dependencies
+        runtimeInputs = with pkgs; [
+          cargo-nextest
+          openssl
+          pkg-config
         ];
-      };
 
-      # Nightly compilator to run miri tests
-      devShells.nightly = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          rustToolchains.nightly
-        ];
-      };
+        # Eval the treefmt configuration
+        treefmtConfig = {
+          projectRootFile = "flake.nix";
 
-      packages = {
-        ci-lints = mkCommandDefault "ci-run-lints";
-        ci-tests = mkCommandDefault "ci-run-tests";
-        ci-semver-checks = mkCommandDefault "ci-run-semver-checks";
-        ci-benchmarks = mkCommandDefault "ci-run-benchmarks";
-        ci-all = mkCommandDefault "ci-run-all";
-        git-install-hooks = pkgs.writeShellScriptBin "install-git-hook"
-          ''
-            echo "-> Installing pre-commit hook"
-            echo "nix flake check" >> "$PWD/.git/hooks/pre-commit"
-            chmod +x "$PWD/.git/hooks/pre-commit"
+          programs = {
+            nixfmt.enable = true;
+            rustfmt = {
+              enable = true;
+              package = rustToolchains.nightly;
+            };
+            beautysh.enable = true;
+            deno.enable = true;
+            taplo.enable = true;
+          };
+        };
+        treefmt = (treefmt-nix.lib.evalModule pkgs treefmtConfig).config.build;
 
-            echo "-> Installing pre-push hook"
-            echo "nix run \".#ci-all\"" >> "$PWD/.git/hooks/pre-push"
-            chmod +x "$PWD/.git/hooks/pre-push"
-          '';
-      };
-    });
+        # CI scripts
+        ci = {
+          tests = pkgs.writeShellApplication {
+            name = "ci-run-tests";
+            runtimeInputs = [ rustToolchains.msrv ] ++ runtimeInputs;
+            text = ''
+              cargo nextest run --workspace --all-targets --no-default-features
+              cargo nextest run --workspace --all-targets --all-features
+
+              cargo test --workspace --doc --no-default-features
+              cargo test --workspace --doc --all-features
+
+              cargo run --example rate_limiter
+              cargo run --example retry
+            '';
+          };
+
+          lints = pkgs.writeShellApplication {
+            name = "ci-run-lints";
+            runtimeInputs = [
+              rustToolchains.stable
+              pkgs.typos
+            ]
+            ++ runtimeInputs;
+            text = ''
+              typos
+              cargo clippy --workspace --all --no-default-features
+              cargo clippy --workspace --all --all-targets --all-features
+              cargo doc --workspace --no-deps --no-default-features
+              cargo doc --workspace --no-deps --all-features
+            '';
+          };
+
+          benchmarks = pkgs.writeShellApplication {
+            name = "ci-run-benchmarks";
+            runtimeInputs = [ rustToolchains.stable ] ++ runtimeInputs;
+            text = ''
+              cargo bench --workspace --all-features
+            '';
+          };
+
+          semver_checks = pkgs.writeShellApplication {
+            name = "ci-run-semver-checks";
+            runtimeInputs = [
+              rustToolchains.msrv
+              pkgs.cargo-semver-checks
+            ]
+            ++ runtimeInputs;
+            text = ''cargo semver-checks'';
+          };
+
+          # Run them all together
+          all = pkgs.writeShellApplication {
+            name = "ci-run-all";
+            runtimeInputs = [
+              ci.lints
+              ci.tests
+              ci.semver_checks
+            ];
+            text = ''
+              ci-run-lints
+              ci-run-tests
+              ci-run-semver-checks
+            '';
+          };
+        };
+      in
+      {
+        # for `nix fmt`
+        formatter = treefmt.wrapper;
+        # for `nix flake check`
+        checks.formatting = treefmt.check self;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = runtimeInputs ++ [
+            rustToolchains.stable
+            treefmt.wrapper
+            ci.all
+          ];
+        };
+
+        # Nightly compiler to run miri tests
+        devShells.nightly = pkgs.mkShell {
+          nativeBuildInputs = [ rustToolchains.nightly ];
+        };
+
+        packages = {
+          inherit (ci)
+            all
+            lints
+            tests
+            semver_checks
+            benchmarks
+            ;
+
+          ci-all = ci.all;
+          ci-lints = ci.lints;
+          ci-tests = ci.tests;
+          ci-semver-checks = ci.semver_checks;
+          ci-benchmarks = ci.benchmarks;
+
+          git-install-hooks = pkgs.writeShellApplication {
+            name = "install-git-hooks";
+            text = ''
+              echo "-> Installing pre-commit hook"
+              echo "nix flake check" >> "$PWD/.git/hooks/pre-commit"
+              chmod +x "$PWD/.git/hooks/pre-commit"
+
+              echo "-> Installing pre-push hook"
+              echo "nix run \".#ci-all\"" >> "$PWD/.git/hooks/pre-push"
+              chmod +x "$PWD/.git/hooks/pre-push"
+            '';
+          };
+        };
+      }
+    );
 }
